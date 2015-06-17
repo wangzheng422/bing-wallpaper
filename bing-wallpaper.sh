a4c8e381cd7bf2d7e927c7e77de0284389ed334b
Microdog <dlangu0393@gmail.com>
2015-06-17 08:44:10 +0800

    Make curl follow HTTP redirect

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,7 +4,7 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://www.bing.com | \
+urls=( $(curl -s http://www.bing.com -L | \
     grep -Eo "url:'.*?'" | \
     sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
     sed -e "s/\\\//g") )
